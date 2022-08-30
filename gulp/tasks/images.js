dd1ac0d46cb9182d137fa81f00fd1418f67aaefd
Aler4 <alerch5666@gmail.com>
2022-08-30 23:19:24 +0300

    tourism form html

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -4,7 +4,6 @@ import imagemin from 'gulp-imagemin';
 export const images = () => {
     return app.gulp.src(app.path.src.images)
         .pipe(app.plugins.newer(app.path.build.images))
-        .pipe(webp())
         .pipe(app.gulp.dest(app.path.build.images))
         .pipe(app.gulp.src(app.path.src.images))
         .pipe(app.plugins.newer(app.path.build.images))
