ceace6188722fde5d00b8591899110d703051028
Aler4 <alerch5556@gmail.com>
2022-09-20 00:41:06 +0300

    form tourism

--- src/js/modules/data-picker.js
+++ src/js/modules/data-picker.js
@@ -2,8 +2,12 @@
 import AirDatepicker from 'air-datepicker';
 export const createPicker = () => {
     let inpts = document.querySelectorAll('input[name="date"]');
+    let date = new Date();
+    let day = (date.getDate() < 10) ? `0${date.getDate()}` : date.getDate();
+    let month = (date.getMonth() < 10) ? `0${date.getMonth()}` : date.getMonth();
     inpts.forEach(el => {
         let id = el.id;
+        el.placeholder = `${day}.${month}.${date.getFullYear()}`;
         if (id !== undefined) {
             let picker = new AirDatepicker(`#${id}`, {
                 inline: false,
--- src/ts/data-picker.ts
+++ src/ts/data-picker.ts
@@ -2,11 +2,16 @@
 import AirDatepicker from 'air-datepicker';
 export const createPicker: Function = ():void => {
     let inpts:NodeList = document.querySelectorAll('input[name="date"]');
+    let date: Date = new Date();
+    let day: number | string = (date.getDate() < 10) ? `0${date.getDate()}` : date.getDate();
+    let month: number | string = (date.getMonth() < 10) ? `0${date.getMonth()}` : date.getMonth();
 
     inpts.forEach(el => {
         let id:string | undefined = (el as Element).id;
+          (el as HTMLInputElement).placeholder = `${day}.${month}.${date.getFullYear()}`;
             if(id !== undefined){
                let picker =  new AirDatepicker(`#${id}`,{
+               
                   inline: false,
                   autoClose:true,
                   isModile: true,
